26a2ec4cd6db942ca2b6560407c513a4a163fe1b
Bjarne Øverli <bjarne.oeverli@gmail.com>
2026-03-07 09:39:45 +0100

    Cycle providers

--- ui/keys.go
+++ ui/keys.go
@@ -1056,6 +1056,7 @@ var keymapEntries = []keymapEntry{
 	{"A", "Queue manager"},
 	{"o", "Open file browser"},
 	{"N", "Navidrome browser"},
+	{"P", "Cycle provider"},
 	{"J", "Jump to time"},
 	{"p", "Playlist manager"},
 	{"i", "Track info / metadata"},
